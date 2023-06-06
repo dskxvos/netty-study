6f6ff623df3409824640e50f9cfe60904d41bdf0
dskxvos <dskxvos@163.com>
2023-06-06 18:17:03 +0800

    1.NIO TimerServer

--- src/main/java/nio/MultiplexerTimerServer.java
+++ src/main/java/nio/MultiplexerTimerServer.java
@@ -2,9 +2,13 @@ package nio;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -50,12 +54,77 @@ public class MultiplexerTimerServer implements Runnable{
                 while (it.hasNext()){
                     selectionKey = it.next();
                     it.remove();
+                    try {
+                        //todo
+                        handlerInput(selectionKey);
+                    }catch (Exception e){
+                        if (selectionKey != null){
+                            selectionKey.cancel();
+                            if (selectionKey.channel() != null){
+                                selectionKey.channel().close();
+                            }
+                        }
+                    }
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+
+        if (selector != null){
+            try {
+                selector.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
 
 
+    private void  handlerInput(SelectionKey key) throws IOException {
+        if (key.isValid()){
+
+            if (key.isAcceptable()){
+                ServerSocketChannel serverSocketChannel = (ServerSocketChannel)key.channel();
+                SocketChannel socketChannel = serverSocketChannel.accept();
+                socketChannel.configureBlocking(false);
+                socketChannel.register(selector,SelectionKey.OP_READ);
+            }
+
+            if (key.isReadable()){
+                SocketChannel socketChannel = (SocketChannel) key.channel();
+                ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+                int readBytes = socketChannel.read(readBuffer);
+                if (readBytes>0){
+                    readBuffer.flip();
+                    byte[] bytes = new byte[readBuffer.remaining()];
+                    readBuffer.get(bytes);
+                    String body = new String(bytes,"UTF-8");
+                    System.out.println("This time server receive order :"+body);
+                    String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body)?new Date(System.currentTimeMillis()).toString():"BAD ORDER";
+                    doWrite(socketChannel,currentTime);
+                }else if (readBytes<0){
+                    key.cancel();
+                    socketChannel.close();
+                }else {
+                    ;
+                }
+
+            }
+
+        }
+    }
+
+
+    private void doWrite(SocketChannel channel,String response) throws IOException {
+
+        if (response != null && response.trim().length()>0){
+            byte[] bytes = response.getBytes(StandardCharsets.UTF_8);
+            ByteBuffer writeBuffer = ByteBuffer.allocate(bytes.length);
+            writeBuffer.put(bytes);
+            writeBuffer.flip();
+            channel.write(writeBuffer);
         }
 
     }
